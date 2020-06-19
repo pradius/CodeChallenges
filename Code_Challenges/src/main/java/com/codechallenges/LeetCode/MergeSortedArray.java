d3881f762263cfdb7a5861ce526d5b0ebf801740
Pradius <josecamilo@outlook.com>
2020-06-19 19:11:36 -0400

    Added Merge Sorted Array

---
+++ Code_Challenges/src/main/java/com/codechallenges/LeetCode/MergeSortedArray.java
@@ -0,0 +1,29 @@
+package com.codechallenges.LeetCode;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(nums2.length < 1) return;
+        int index2 = 0;
+
+        for (int i = m; i < nums1.length; i++){
+            nums1[i] = nums2[index2];
+            index2++;
+        }
+        Arrays.sort(nums1);
+    }
+
+    public static void main(String[] args) {
+
+        //Test 1
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+        System.out.println("Test 1: Expected [1,2,2,3,5,6]");
+        merge(nums1, m,  nums2, n);
+        Arrays.stream(nums1).forEach(System.out::println);
+    }
+}
